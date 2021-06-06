a579d453012799c4454512a1901b89fe72f9fe5f
NoorinSakhi <70807500+NoorinSakhi@users.noreply.github.com>
2021-06-06 11:07:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-window.onload = function(){
-    document.getElementById('menulink').onclick = function(){
+window.onload = function () {
+    document.getElementById('menulink').onclick = function () {
         var menu = document.getElementById('menu');
-        if(menu.className != 'shownmenu'){
+        if (menu.className != 'shownmenu') {
             menu.className = 'shownmenu';
         }
         else {
@@ -11,38 +11,37 @@ window.onload = function(){
 }
 
 function check_web_storage_support() {
-    if(typeof(Storage) !== "undefined") {
-        return(true);
+    if (typeof (Storage) !== "undefined") {
+        return (true);
     }
     else {
-        alert("Web storage undefined!");
-        return(false);
+        alert("Web storage undefined! This means we can't save your note! 🤔");
+        return (false);
     }
 }
 
-
-function display_saved_note(){
-    if(check_web_storage_support() == true){
+function display_saved_note() {
+    if (check_web_storage_support() == true) {
         result = localStorage.getItem('note');
     }
-    if(result === null) {
+    if (result === null) {
         result = "No Note Saved.";
     }
     document.getElementById('area').value = result;
 }
 
 function save() {
-    if(check_web_storage_support() == true){
+    if (check_web_storage_support() == true) {
         var area = document.getElementById("area");
-        if(area.value != '') {
-            localStorage.setItem("note", area.value);
+        if (area.value != '') {
+            localStorage.setItem("note", area.value) + alert("Note Saved! Please do not reload this page, to keep your notes here. You may close this app, as it is safe when you reopen.");
         }
         else {
-            alert ("Nothing To Save");
+            alert("Type something in order to save 📝");
         }
     }
 }
 
-function clear(){
+function clear() {
     document.getElementById('area').value = "";
 }
